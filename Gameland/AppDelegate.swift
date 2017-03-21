e4c0e21a3d1d9f67393059fec6396c266cbeca09
fraanii94 <frn94@live.com>
2017-03-21 13:17:47 +0100

    Created HomeViewController and added the hackersWeek image, I also added a label and i have animated it to show how animations and closures work, when the user tap the image, the GamesTableViewController is presented

--- Gameland/AppDelegate.swift
+++ Gameland/AppDelegate.swift
@@ -15,29 +15,51 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        // Acaba de iniciarse la aplicación, no significa que ya esté mostrando algo, todo lo contrario
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        window?.rootViewController = HomeViewController()
+        
+        window?.makeKeyAndVisible()
+        
+        
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
+        
+        // Se interrumpe la ejecucion de la app por una interrupcion o pulsamos el boton HOME
+        
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
+        // La aplicacion se va a ejecutar en segundo plano
+        
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
+        // LA aplicacion va a entrar en primer plano
+        
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
+        
+        // La aplicacion ha vuelto a estar activa, la vemos en pantalla
+        
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
+        
+        // Se va a eliminar la aplicacion
+        
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
---
+++ Gameland/HomeViewController.swift
@@ -0,0 +1,47 @@
+//
+//  HomeViewController.swift
+//  Gameland
+//
+//  Created by Fran Navarro on 21/3/17.
+//  Copyright © 2017 frndev. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var logoImageView: UIImageView!
+    
+    @IBOutlet weak var infoLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let tap = UITapGestureRecognizer(target: self, action: #selector(userDidTapOnImageView(_:)))
+        
+        self.logoImageView.addGestureRecognizer(tap)
+        self.logoImageView.isUserInteractionEnabled = true
+        
+        animateInfoLabelWith(alpha:0)
+        
+    }
+
+    func userDidTapOnImageView(_ sender: UITapGestureRecognizer) {
+        
+        let navController = UINavigationController(rootViewController: GamesTableViewController())
+        
+        self.present(navController, animated: true, completion: nil)
+        
+    }
+    
+    func animateInfoLabelWith(alpha a: CGFloat) {
+        
+        UIView.animate(withDuration: 0.85, animations: {
+            self.infoLabel.alpha = a
+            }) { (finished) in
+                self.animateInfoLabelWith(alpha: a == 0 ? 1 : 0)
+        }
+        
+    }
+    
+
+}
